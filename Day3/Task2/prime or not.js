7caf4add89afb54d32e91d78ac8e928b80d6e869
veerababu16 <129863638+veerababu16@users.noreply.github.com>
2024-03-20 14:56:01 +0530

    Update prime or not.js

--- Day3/Task2/prime or not.js
+++ Day3/Task2/prime or not.js
@@ -1,1 +1,14 @@
-
+function abc(n){
+    for(i=2;i<n;i++){
+    b=true
+    for(j=2;j<i;j++){
+        if(i%j==0){
+            b=false
+        }
+    }
+    if(b==true){
+        console.log(i);
+    }
+    }
+}
+abc(58)
